47064b3d0bf38fd57a9e9ae22c94bc6f55c95b80
Zach Rice <zachary.rice@modolabs.com>
2017-11-22 18:36:24 -0500

    Add update times to book.

--- actors/book_actor.rb
+++ actors/book_actor.rb
@@ -4,6 +4,8 @@ class BookActor
   attr_reader :book
   attr_reader :sorted_prices
   attr_reader :pair
+  attr_reader :last_bid_update
+  attr_reader :last_ask_update
 
   def initialize(pair)
     @book = {
@@ -16,17 +18,38 @@ class BookActor
       asks: [],
     }
 
+    @last_bid_update = 0
+    @last_ask_update = 0
+
     @pair = pair
+  end
+
+  def time_since_ask
+    Time.now.to_i - last_ask_update
+  end
+
+  def time_since_bid
+    Time.now.to_i - last_bid_update
+  end
+
+  def current_ask
+    @sorted_prices[:asks][0]
+  end
+
+  def current_bid
+    @sorted_prices[:bids][0]
   end
 
   def add_bid!(price:, volume:)
     @book[:bids][price] = volume
     sort_bids!
+    @last_bid_update = Time.now.to_i
   end
 
   def add_ask!(price:, volume:)
     @book[price] = volume
     sort_asks!
+    @last_ask_update = Time.now.to_i
   end
 
   def delete!(side:, price:)
